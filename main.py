9055640d4f970089f78c13d6b80303e18c9b1ca7
Harish <74132905+questhunter@users.noreply.github.com>
2021-12-08 18:21:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,38 +4,40 @@ import threading
 import download
 import set_path
 
+
 class GUI:
-    def __init__(self,root=None):
-    # BackGround
+    def __init__(self, root=None):
+        # BackGround
         self.bg = ImageTk.PhotoImage(file=r"img\bg.jpg")
         self.label = tkinter.Label(root, image=self.bg)
         self.label.place(x=0, y=0, relwidth=1, relheight=1)
         #
 
         # entry
-        
+
         self.entry = tkinter.Entry(root, width=40, font=('Gotham Book', 16))
         self.entry.place(x=178, y=265)
         #
-
 
         # download button
         self.dl_btn_img = tkinter.PhotoImage(file=r"img\download button.png")
 
         self.dl_btn_img_label = tkinter.Label(image=self.dl_btn_img)
 
-
-        self.dl_btn = tkinter.Button(root, image=self.dl_btn_img, borderwidth=0, bg='black', activebackground='black', command=lambda:threading.Thread(target=download.get_link(self)).start())
+        self.dl_btn = tkinter.Button(root, image=self.dl_btn_img, borderwidth=0, bg='black',
+                                     activebackground='black', command=lambda: threading.Thread(target=download.get_link, args=[self]).start())
         self.dl_btn.place(x=347, y=365)
         #
 
         # path_button
         self.pth_btn_img = ImageTk.PhotoImage(file=r"img\path 4.jpg")
 
-        self.pth_btn = tkinter.Button(root, image=self.pth_btn_img, borderwidth=0, bg='black', activebackground='black', command=set_path.get_path)
+        self.pth_btn = tkinter.Button(root, image=self.pth_btn_img, borderwidth=0,
+                                      bg='black', activebackground='black', command=set_path.get_path)
         self.pth_btn.place(x=670, y=265)
 
-if __name__ == "__main__": 
+
+if __name__ == "__main__":
     root = tkinter.Tk()
     root.title("Spotify Downloader")
     root.iconbitmap(default=r'img\spotify.ico')
