04dfff6c0b58d3449676586d79f9be70180ac238
Harish <74132905+questhunter@users.noreply.github.com>
2021-12-14 13:05:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import tkinter
 from PIL import ImageTk
-import threading
 import download
 import set_path
 
@@ -19,11 +18,18 @@ class GUI:
         self.label.place(x=0, y=0, relwidth=1, relheight=1)
         #
 
+        GUI.center(self.root)
+
         # entry
 
-        self.entry = tkinter.Entry(root, width=40, font=('Gotham Book', 16))
-        self.entry.place(x=178, y=265)
+        self.entry = tkinter.Entry(root, font=('Gotham Book', 16))
+        self.entry.place(relwidth=0.55, x=178, y=265)
         #
+
+        # right  click menu
+        GUI.make_menu(root)
+        self.entry.bind_class(
+            "Entry", "<Button-3>", GUI.show_menu)
 
         # download button
         self.dl_btn_img = tkinter.PhotoImage(file=r"img\download button.png")
@@ -44,6 +50,40 @@ class GUI:
 
         self.root.resizable(False, False)
         self.root.mainloop()
+
+    def make_menu(w):
+        global the_menu
+        the_menu = tkinter.Menu(w, tearoff=0)
+        the_menu.add_command(label="Cut")
+        the_menu.add_command(label="Copy")
+        the_menu.add_command(label="Paste")
+
+    def show_menu(e):
+        w = e.widget
+        the_menu.entryconfigure("Cut",
+                                command=lambda: w.event_generate("<<Cut>>"))
+        the_menu.entryconfigure("Copy",
+                                command=lambda: w.event_generate("<<Copy>>"))
+        the_menu.entryconfigure("Paste",
+                                command=lambda: w.event_generate("<<Paste>>"))
+        the_menu.tk.call("tk_popup", the_menu, e.x_root, e.y_root)
+
+    def center(win):
+        """
+        centers a tkinter window
+        :param win: the main window or Toplevel window to center
+        """
+        win.update_idletasks()
+        width = win.winfo_width()
+        frm_width = win.winfo_rootx() - win.winfo_x()
+        win_width = width + 2 * frm_width
+        height = win.winfo_height()
+        titlebar_height = win.winfo_rooty() - win.winfo_y()
+        win_height = height + titlebar_height + frm_width
+        x = win.winfo_screenwidth() // 2 - win_width // 2
+        y = win.winfo_screenheight() // 2 - win_height // 2
+        win.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+        win.deiconify()
 
 
 if __name__ == "__main__":
